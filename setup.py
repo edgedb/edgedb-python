01a73ba188ca5d7d639f91f9bfa58ebba7f6e5b7
Elvis Pranskevichus <elvis@magic.io>
2019-01-25 16:16:32 -0500

    Make sure all sub-packages are included in the distribution

--- setup.py
+++ setup.py
@@ -156,8 +156,7 @@ setuptools.setup(
     author_email='hello@magic.io',
     url='https://github.com/edgedb/edgedb-python',
     license='Apache License, Version 2.0',
-    packages=['edgedb'],
-    provides=['edgedb'],
+    packages=setuptools.find_namespace_packages(include=['edgedb*']),
     zip_safe=False,
     include_package_data=True,
     ext_modules=[
