47274e9d61635c724f49799a0d249cd58ab75100
Elvis Pranskevichus <elvis@edgedb.com>
2019-12-08 11:34:49 -0500

    Fix tests on macOS
    Use the correct latest nightly snapshot URL of EdgeDB on macOS.

--- .github/workflows/install-edgedb.sh
+++ .github/workflows/install-edgedb.sh
@@ -3,7 +3,7 @@
 set -Exeo pipefail
 
 if [[ "${OS_NAME}" == macos* ]]; then
-    wget https://packages.edgedb.com/macos/edgedb-${SLOT}-latest.pkg \
+    wget https://packages.edgedb.com/macos/edgedb-${SLOT}_latest.nightly.pkg \
         -O edgedb.pkg -nv
     sudo env _EDGEDB_INSTALL_SKIP_BOOTSTRAP=1 installer \
         -dumplog -verbose -pkg "$(pwd)/edgedb.pkg" -target /
