9d91e14e66a947e0439bd6b8225e965b621f5584
Elvis Pranskevichus <elvis@edgedb.com>
2020-02-19 14:04:29 -0500

    edgedb-python v0.8.0a1
    Bump for protocol version changes.

--- edgedb/_version.py
+++ edgedb/_version.py
@@ -28,4 +28,4 @@
 # supported platforms, publish the packages on PyPI, merge the PR
 # to the target branch, create a Git tag pointing to the commit.
 
-__version__ = '0.7.2a2'
+__version__ = '0.8.0a1'
