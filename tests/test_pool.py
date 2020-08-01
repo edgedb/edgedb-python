26032ff5577523ee7d3b6ec65caa94d62d72cce4
Eugene <fogaprod@gmail.com>
2020-08-01 21:31:52 +0300

    Working implementation

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -516,3 +516,17 @@ class TestPool(tb.AsyncQueryTestCase):
 
         await pool_task
         await pool.aclose()
+
+    async def test_pool_properties(self):
+        min_size = 1
+        max_size = 2
+
+        pool = await self.create_pool(min_size=min_size, max_size=max_size)
+        self.assertEqual(pool.min_size, min_size)
+        self.assertEqual(pool.max_size, max_size)
+        self.assertEqual(pool.free_size, max_size)
+
+        async with pool.acquire() as _:
+            self.assertEqual(pool.free_size, max_size - 1)
+
+        await pool.aclose()
