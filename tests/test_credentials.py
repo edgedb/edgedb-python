b354b9b66f850dcc48453a3c9df355f0e5a3cecc
Elvis Pranskevichus <elvis@edgedb.com>
2020-08-26 13:41:17 -0700

    Fix credentials tests
    Use absolute path to read the credentials file.

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,11 +1,33 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2020-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+import pathlib
 import unittest
+
 from edgedb import credentials
 
 
 class TestCredentials(unittest.TestCase):
 
-    def test_read(self):
-        creds = credentials.read_credentials('tests/credentials1.json')
+    def test_credentials_read(self):
+        creds = credentials.read_credentials(
+            pathlib.Path(__file__).parent / 'credentials1.json')
         self.assertEqual(creds, {
             'database': 'test3n',
             'password': 'lZTBy1RVCfOpBAOwSCwIyBIR',
@@ -13,11 +35,11 @@ class TestCredentials(unittest.TestCase):
             'user': 'test3n',
         })
 
-    def test_empty(self):
+    def test_credentials_empty(self):
         with self.assertRaisesRegex(ValueError, '`user` key is required'):
             credentials.validate_credentials({})
 
-    def test_port(self):
+    def test_credentials_port(self):
         with self.assertRaisesRegex(ValueError, 'invalid `port` value'):
             credentials.validate_credentials({
                 'user': 'u1',
@@ -42,7 +64,7 @@ class TestCredentials(unittest.TestCase):
                 'port': 65536,
             })
 
-    def test_extra_key(self):
+    def test_credentials_extra_key(self):
         creds = credentials.validate_credentials(dict(
             user='user1',
             some_extra_data='test',
