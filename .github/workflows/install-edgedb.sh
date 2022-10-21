af05117e7267c0f266fdbab2470425d003e1a6e9
Fantix King <fantix.king@gmail.com>
2022-10-21 11:11:43 -0400

    Fix codegen test in release CI (#383)
    * Use portable server in release CI

--- .github/workflows/install-edgedb.sh
+++ .github/workflows/install-edgedb.sh
@@ -10,14 +10,10 @@ curl -fL "${srv}/dist/x86_64-unknown-linux-musl/edgedb-cli" \
 
 chmod +x "/usr/local/bin/edgedb"
 
-# XXX: replace this with edgedb server install once that supports
-#      portable builds.
-curl -fL "${srv}/archive/x86_64-unknown-linux-gnu/edgedb-server-1.0-rc.2%2Bc328744.tar.gz" \
-    > "/tmp/edgedb-server.tar.gz"
-
-mkdir -p "/opt/edgedb-server"
-tar -xz --strip-components=1 -C "/opt/edgedb-server" -f "/tmp/edgedb-server.tar.gz"
-ln -s "/opt/edgedb-server/bin/edgedb-server" "/usr/local/bin/edgedb-server"
-
 useradd --shell /bin/bash edgedb
+
+su -l edgedb -c "edgedb server install"
+ln -s $(su -l edgedb -c "edgedb server info --latest --bin-path") \
+    "/usr/local/bin/edgedb-server"
+
 edgedb-server --version
