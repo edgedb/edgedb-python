c300b7af96079b49c65a723e372090f7880a62d9
Fantix King <fantix.king@gmail.com>
2022-11-02 17:55:50 -0400

    Bump version to 2.0

--- edgedb/_version.py
+++ edgedb/_version.py
@@ -28,4 +28,4 @@
 # supported platforms, publish the packages on PyPI, merge the PR
 # to the target branch, create a Git tag pointing to the commit.
 
-__version__ = '1.0.0'
+__version__ = '2.0.0.dev1'
