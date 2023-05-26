a0d5d1e03c7648a3be45128e9a861a4035249ba1
Fantix King <fantix.king@gmail.com>
2023-05-26 16:02:28 -0400

    Update release CI

--- .github/workflows/install-edgedb.sh
+++ .github/workflows/install-edgedb.sh
@@ -5,15 +5,20 @@ shopt -s nullglob
 
 srv="https://packages.edgedb.com"
 
-curl -fL "${srv}/dist/x86_64-unknown-linux-musl/edgedb-cli" \
-    > "/usr/local/bin/edgedb"
+curl -fL "${srv}/dist/$(uname -m)-unknown-linux-musl/edgedb-cli" \
+    > "/usr/bin/edgedb"
 
-chmod +x "/usr/local/bin/edgedb"
+chmod +x "/usr/bin/edgedb"
 
-useradd --shell /bin/bash edgedb
+if command -v useradd >/dev/null 2>&1; then
+    useradd --shell /bin/bash edgedb
+else
+    # musllinux/alpine doesn't have useradd
+    adduser -s /bin/bash -D edgedb
+fi
 
 su -l edgedb -c "edgedb server install"
 ln -s $(su -l edgedb -c "edgedb server info --latest --bin-path") \
-    "/usr/local/bin/edgedb-server"
+    "/usr/bin/edgedb-server"
 
 edgedb-server --version
