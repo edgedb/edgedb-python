56aba5b4ed2f42eaa6a81e15a4faca9a46bd51aa
Fantix King <fantix.king@gmail.com>
2023-09-22 17:29:00 -0400

    Allow different codegen assertions based on server version

--- tests/test_codegen.py
+++ tests/test_codegen.py
@@ -27,6 +27,9 @@ import tempfile
 from edgedb import _testbase as tb
 
 
+ASSERT_SUFFIX = os.environ.get("EDGEDB_TEST_CODEGEN_ASSERT_SUFFIX", ".assert")
+
+
 class TestCodegen(tb.AsyncQueryTestCase):
     SETUP = '''
         create extension pgvector;
@@ -109,7 +112,9 @@ class TestCodegen(tb.AsyncQueryTestCase):
         )
 
         for f in cwd.rglob("*.py"):
-            a = f.with_suffix(".py.assert")
+            a = f.with_suffix(f".py{ASSERT_SUFFIX}")
+            if not a.exists():
+                a = f.with_suffix(".py.assert")
             self.assertEqual(f.read_text(), a.read_text(), msg=f.name)
         for a in cwd.rglob("*.py.assert"):
             f = a.with_suffix("")
