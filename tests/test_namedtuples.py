518351722dbd8ccca9983f5338d65d0078b88bb4
Michael J. Sullivan <sully@msully.net>
2024-01-31 14:16:36 -0800

    Support passing dicts and namedtuples for namedtuple arguments
    We support dicts (and other mappings), and for named tuples we do
    lookup by name instead of by position.
    
    Fixes #374.

---
+++ tests/test_namedtuples.py
@@ -0,0 +1,38 @@
+#
+# This source file is part of the EdgeDB open source project.
+#
+# Copyright 2019-present MagicStack Inc. and the EdgeDB authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+from collections import namedtuple, UserDict
+
+from edgedb import _testbase as tb
+
+
+class TestNamedTupleTypes(tb.SyncQueryTestCase):
+
+    async def test_namedtuple_01(self):
+        NT1 = namedtuple('NT2', ['x', 'y'])
+        NT2 = namedtuple('NT2', ['y', 'x'])
+
+        ctors = [dict, UserDict, NT1, NT2]
+        for ctor in ctors:
+            val = ctor(x=10, y='y')
+            res = self.client.query_single('''
+                select <tuple<x: int64, y: str>>$0
+            ''', val)
+
+            self.assertEqual(res, (10, 'y'))
